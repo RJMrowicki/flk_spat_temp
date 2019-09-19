c5d7dfb793d8c2074eb23c845a0185e20ab15030
Rob Mrowicki <robert.mrowicki@gmail.com>
2019-09-19 11:51:32 +0000

    plot taxon maps for most recent year group only

--- scripts/02_plot_distribution_maps.R
+++ scripts/02_plot_distribution_maps.R
@@ -46,30 +46,34 @@ par(mar = rep(0.1, 4))  # narrow outer margins
 
 # create plots:
 for (i in taxa) {  # for each taxon,
-  
-  # plot raster cell underlay (as polygons):
-  plot(
-    rasterToPolygons(flk_coast_raster),
-    border = grey(0.75), lwd = 1
-  )
-  
-  # add simplified flk polygon shapefile:
-  plot(
-    shp_flk_simple, add = TRUE,
-    lwd = 0.5
-  )
-  
-  # add taxon raster squares:
-  plot(
-    rasterToPolygons(taxa_rasters[[i]]), add = TRUE,
-    border = NA, col = adjustcolor("royalblue", alpha = 0.8)
-  )
-  
-  # add taxon name in top right corner:
-  legend(
-    "topright", legend = i, bty = "n", cex = 1
-  )
-  
+  # only if coordinates are not NULL (NB -- **most recent** year group):
+  if (!is.null(taxa_rasters[[i]][[year_grps[length(year_grps)]]])) {
+    
+    # plot raster cell underlay (as polygons):
+    plot(
+      rasterToPolygons(flk_coast_raster),
+      border = grey(0.75), lwd = 1
+    )
+    
+    # add simplified flk polygon shapefile:
+    plot(
+      shp_flk_simple, add = TRUE,
+      lwd = 0.5
+    )
+    
+    # add taxon raster squares:
+    plot(
+      # (NB -- **most recent** year group only)
+      rasterToPolygons(taxa_rasters[[i]][[year_grps[length(year_grps)]]]),
+      add = TRUE, border = NA, col = adjustcolor("royalblue", alpha = 0.8)
+    )
+    
+    # add taxon name in top right corner:
+    legend(
+      "topright", legend = i, bty = "n", cex = 1
+    )
+    
+  }
 }
 
 
